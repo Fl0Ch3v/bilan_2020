31c9891ec61ffc00acc08d8480a82f22f4c9e8cb
Fl0Ch3v <65029165+Fl0Ch3v@users.noreply.github.com>
2020-12-02 16:02:40 +0100

    Update mesFonctions.js

--- mesFonctions.js
+++ mesFonctions.js
@@ -191,7 +191,7 @@ function initialize() {
 						});
 										});	
 
-		$.getJSON("pts.geojson",function(data_point){
+		$.getJSON("data/pts.geojson",function(data_point){
 			var styleIcon = L.icon({
 			iconUrl: 'icon.png',
 			iconSize: [15,20]
